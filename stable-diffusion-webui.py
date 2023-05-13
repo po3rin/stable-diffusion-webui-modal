12b7567ad4d01c205ebb3aa7a9c06da631ce37e6
Hiromu Nakamura <hiromu-nakamura@m3.com>
2023-05-14 02:53:50 +0900

    extension

--- stable-diffusion-webui.py
+++ stable-diffusion-webui.py
@@ -1,38 +1,35 @@
-from colorama import Fore
-from pathlib import Path
-
-import modal
+import os
+import shlex
 import shutil
 import subprocess
 import sys
-import shlex
-import os
+from pathlib import Path
 
+import modal
+from colorama import Fore
 
 stub = modal.Stub("stable-diffusion-webui")
 volume_main = modal.SharedVolume().persist("stable-diffusion-webui-main")
 
 webui_dir = "/content/stable-diffusion-webui"
-webui_model_dir = webui_dir + "/models/Stable-diffusion/"
+sd_dir = webui_dir + "/models/Stable-diffusion/"
 
 # 読み込むモデルのリスト
-model_ids = [
-    {
-        "repo_id": "hakurei/waifu-diffusion-v1-4",
-        "model_path": "wd-1-4-anime_e1.ckpt",
-        "config_file_path": "wd-1-4-anime_e1.yaml",
-    } 
-]
+model_ids = [{
+    "repo_id": "hakurei/waifu-diffusion-v1-4",
+    "model_path": "wd-1-4-anime_e1.ckpt",
+    "config_file_path": "wd-1-4-anime_e1.yaml",
+}]
 
 @stub.function(
-    image=modal.Image.from_dockerhub("python:3.10-slim")
-    .apt_install(
-        "gcc", "git", "libgl1-mesa-dev", "libglib2.0-0", "libsm6", "libxrender1", "libxext6", "libcairo2-dev", "libcairo2"
-    )
-    .run_commands(
-        "pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers"
-    )
-    .pip_install(
+    image=modal.Image.from_dockerhub("python:3.10-slim").apt_install(
+        "gcc", "git", "libgl1-mesa-dev", "libglib2.0-0", "libsm6",
+        "libxrender1", "libxext6", "libcairo2").
+    run_commands(
+        "pip install -e git+https://github.com/CompVis/"\
+            "taming-transformers.git@master"\
+            "#egg=taming-transformers"
+    ).pip_install(
         "blendmodes==2022",
         "transformers==4.25.1",
         "accelerate==0.12.0",
@@ -73,57 +70,64 @@ model_ids = [
         "gdown",
         "huggingface_hub",
         "colorama",
-    )
-    .pip_install("git+https://github.com/mlfoundations/open_clip.git@bb6e834e9c70d9c27d0dc3ecedeebeaeb1ffad6b"),
+    ).pip_install(
+        "git+https://github.com/mlfoundations/open_clip.git"\
+            "@bb6e834e9c70d9c27d0dc3ecedeebeaeb1ffad6b"
+    ),
     secret=modal.Secret.from_name("huggingface-secret"),
     shared_volumes={webui_dir: volume_main},
     gpu="a10g",
     timeout=6000,
 )
-
-
-def download_hf_file(repo_id, filename) -> str:
-    """
-    Hugging faceからファイルをダウンロードしてくる関数
-    """
-    from huggingface_hub import hf_hub_download
-
-    download_dir = hf_hub_download(repo_id=repo_id, filename=filename)
-    return download_dir
-
-
 async def run_stable_diffusion_webui():
-    webui_dir_path = Path(webui_model_dir)
+    webui_dir_path = Path(sd_dir)
     if not webui_dir_path.exists():
-        subprocess.run(f"git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui  {webui_dir}", shell=True)
+        subprocess.run(
+            f"git clone https://github.com/AUTOMATIC1111/"\
+                f"stable-diffusion-webui  {webui_dir}",
+            shell=True
+        )
+
+    def download_hf_file(repo_id, filename) -> str:
+        """
+        Hugging faceからファイルをダウンロードしてくる関数
+        """
+        from huggingface_hub import hf_hub_download
+
+        download_dir = hf_hub_download(repo_id=repo_id, filename=filename)
+        return download_dir
 
     for model_id in model_ids:
-        if not Path(webui_model_dir + model_id["model_path"]).exists():
+        if not Path(sd_dir + model_id["model_path"]).exists():
             model_downloaded_dir = download_hf_file(
                 model_id["repo_id"],
                 model_id["model_path"],
             )
-            shutil.copy(model_downloaded_dir, webui_model_dir + os.path.basename(model_id["model_path"]))
+            shutil.copy(model_downloaded_dir,
+                        sd_dir + os.path.basename(model_id["model_path"]))
 
         if "config_file_path" not in model_id:
-          continue
+            continue
 
-        if not Path(webui_model_dir + model_id["config_file_path"]).exists():
-            # コンフィグのダウンロード＆コピー
+        if not Path(sd_dir + model_id["config_file_path"]).exists():
             config_downloaded_dir = download_hf_file(
-                model_id["repo_id"], model_id["config_file_path"]
-            )
-            shutil.copy(config_downloaded_dir, webui_model_dir + os.path.basename(model_id["config_file_path"]))
+                model_id["repo_id"], model_id["config_file_path"])
+            shutil.copy(
+                config_downloaded_dir,
+                sd_dir + os.path.basename(model_id["config_file_path"]))
 
         print(Fore.GREEN + model_id["repo_id"] + "のセットアップが完了しました！")
 
-    # WebUIを起動
-    from launch import start, prepare_environment
     sys.path.append(webui_dir)
     sys.argv += shlex.split("--skip-install --xformers")
     os.chdir(webui_dir)
+    from launch import prepare_environment, start
+
     prepare_environment()
-    sys.argv = shlex.split("--gradio-debug --share --xformers --enable-insecure-extension-access")
+    sys.argv = shlex.split(
+        "--gradio-debug --share --xformers"\
+            " --enable-insecure-extension-access"
+    )
     start()
 
 
